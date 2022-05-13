88ad7c5452b90b3aa5deffece403ecf0652c7175
Mack Kaputo <mackkaputo@gmail.com>
2022-05-13 18:07:19 +0200

    single univ DAO added

--- src/routes/universitiesRoutes.js
+++ src/routes/universitiesRoutes.js
@@ -10,15 +10,33 @@ router.get("/", async (req, res) => {
     return res.status(200).json(response)
 })
 
+
+
 router.get("/:id", async (req, res) => {
 
-    let response = await UniversitiesDAO.getUniversityById(req.params.id)
+    try {
 
-    if(response.error) {
-        return res.status(404).json({ message: response.message})
+        let response = await UniversitiesDAO.getUniversityById(req.params.id)
+
+        if(response.error) {
+            return res.status(404).json({ message: response.message})
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: response
+        })
+
+    } catch (error) {
+
+        console.error(`Something went wrong while processing university data`, error)
+
+        return res.status(500).json({
+            error: true,
+            message: "Sorry, something went wrong"
+        })
     }
 
-    return res.status(200).json(response)
 })
 
 
