d134d838ab5c7d60fcbe7abaaff3350fc3404365
Mack Kaputo <mackkaputo@gmail.com>
2022-05-26 23:31:11 +0200

    init rating post DAO

---
+++ src/dao/ratingPostsDAO.js
@@ -0,0 +1,33 @@
+import { ObjectId } from "mongodb"
+
+let universities_experience_db
+let universitiesCollection
+
+export default class ratingPostsDAO {
+    static async injectDB(conn) {
+
+        if (universitiesCollection) { return }
+
+        try {
+            universities_experience_db = await conn.db(process.env.DB_UNIVERSITY_EXPERIENCE_NAME)
+            universitiesCollection = await conn.db(process.env.DB_UNIVERSITY_EXPERIENCE_NAME).collection("universities")
+            
+            //! For testing purposes only
+            this.universitiesCollection = universitiesCollection
+        } catch (error) {
+            console.error(`Unable to connect to the UniversitiesDAO: ${error}`)
+        }
+    }
+
+    static async getUniversities(){
+        //TODO: try catch ad normalize response with { error: Boolean, data: response data}
+        const universities = await universitiesCollection
+            .find({})
+            .toArray()
+        console.log("Getting universities from db...")
+        
+        return universities
+    }
+
+  
+}
