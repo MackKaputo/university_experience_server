da440a630d798cd23934a6739bedd67ff8e34eb9
Mack Kaputo <mackkaputo@gmail.com>
2023-01-29 01:05:50 +0200

    initial router service pattern set

--- univ-nest/src/auth/auth.controller.ts
+++ univ-nest/src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller({})
+@Controller('auth')
 export class AuthController {
+  /**
+   * With th code below, nest will handle how to create an instance of the AuthService class,
+   * So we don't have to manage where and when it's created. ( const service = new authService())
+   * We basically do injection of the AuthService
+   */
   constructor(private authService: AuthService) {}
+  @Post('signup')
+    signup() {
+      return this.authService.signup()
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin()
+  }
 }
--- univ-nest/src/auth/auth.service.ts
+++ univ-nest/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+    signup() {
+        return { msg: "Signed up friend!"}
+    }
+
+    signin() {
+        return { masg: "Signed in boss"}
+    }
+}
