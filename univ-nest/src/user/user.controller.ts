b7b162bbf96449d6165b930080057f9742da98b9
Mack Kaputo <mackkaputo@gmail.com>
2023-02-16 22:29:36 +0200

    guard and custom decorator added

--- univ-nest/src/user/user.controller.ts
+++ univ-nest/src/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(JwtGuard)
     @Get('me')
-    getMe(@Req() req: Request) {
-        console.log("req.user: ", req.user)
-        return req.user
+    getMe(@GetUser() user: unknown ) {
+        console.log("req.user: ", user)
+        return user
     }
 }
